43368e643378d6821966c22658fa2836428b2e26
Marco Bosquez <60762402+MarcoBosglez@users.noreply.github.com>
2021-03-26 13:22:33 -0600

    Add files via upload

---
+++ AmmoText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoText : MonoBehaviour
+{
+    public Text ammoText;
+
+    public void UpdateAmmo(int currentAmmo, int maxAmmo)
+    {
+        ammoText.text = currentAmmo + "/" + maxAmmo;
+    }
+}
---
+++ fpsCounter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class fpsCounter: MonoBehaviour
+{
+    [Header("FPS Data")]
+    private float hudRefreshRate = 1f;
+    private float timer;
+    public Text fpsText;
+
+    public void Update()
+    {
+        if (Time.unscaledTime > timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = "FPS: " + fps;
+            timer = Time.unscaledTime + hudRefreshRate;
+        }
+    }
+
+}

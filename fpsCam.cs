0bd71b93bcd84142855e361d58992f523b9e6b06
Marco Bosquez <60762402+MarcoBosglez@users.noreply.github.com>
2021-03-26 13:20:18 -0600

    Add files via upload

---
+++ fpsCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsCam : MonoBehaviour
+{
+    public float weaponRange = 50f;
+    private Camera fpsCamera;
+
+    void Start()
+    {
+        fpsCamera = GetComponentInParent<Camera>();
+    }
+
+    void Update()
+    {
+        Vector3 lineOrigin = fpsCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        Debug.DrawRay(lineOrigin, fpsCamera.transform.forward * weaponRange, Color.green);
+    }
+}

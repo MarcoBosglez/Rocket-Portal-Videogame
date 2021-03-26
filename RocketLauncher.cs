f62a22322b3913d5d8bd683588c66d0f2a992ade
Marco Bosquez <60762402+MarcoBosglez@users.noreply.github.com>
2021-03-26 13:21:05 -0600

    Add files via upload

---
+++ RocketLauncher.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    // Weapon Stats
+    private int currentAmmo = 4;
+    private int clipAmount;
+    public int maxAmmo = 20;
+
+    public float reloadTime = 0.7f;
+    private float lastFireTime;
+
+    [Header("Projectile Variables")]
+    // Projectile
+    public Rigidbody rocket;
+    public float rocketSpeed = 50f;
+
+    [Header("Manager & Animator")]
+    // Manager & Animator
+    public Animator animator;
+    private AmmoText gameplayManager;
+
+    [Header("States")]
+    // States
+    private bool isReloading = false;
+
+    private void Awake()
+    {
+        gameplayManager = GameObject.FindObjectOfType<AmmoText>();
+    }
+
+    private void Start()
+    {
+        clipAmount = currentAmmo;
+    }
+
+    void Update()
+    {
+        gameplayManager.UpdateAmmo(currentAmmo, maxAmmo);
+
+        if (isReloading)
+            return;
+
+        if (currentAmmo == 0) {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo != clipAmount)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time > (lastFireTime + reloadTime) && currentAmmo > 0)
+        {
+            currentAmmo--;
+
+            Rigidbody clonedRocket;
+            clonedRocket = Instantiate(rocket, transform.position, transform.rotation);
+
+            clonedRocket.velocity = transform.TransformDirection(Vector3.forward * rocketSpeed);
+            lastFireTime = Time.time;
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        animator.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+
+        int mun = 4 - currentAmmo;
+
+        currentAmmo = clipAmount;
+        maxAmmo -= mun;
+        Debug.Log("Max Ammo: " + maxAmmo);
+ 
+        isReloading = false;
+    }
+}
---
+++ RocketScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    */
+
+    [Header("Projectile Variables")]
+    public float velocity;
+    public float liveTime;
+
+    [Header("Damage Stats")]
+    public int damage = 35;
+    public float explosionForce = 15;
+    public float blastRadius = 5;
+
+    public GameObject explosion;
+    Rigidbody rb;
+
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * velocity;
+
+        Invoke("Explode", liveTime);
+    }
+    
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            Explode();
+        }
+        else
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+    */
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            Explode();
+        }
+        else if (other.gameObject.layer == 9)
+        {
+            // Salto desde el muro Explode()
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+    
+
+    public void Explode()
+    {
+        Collider[] players = Physics.OverlapSphere(transform.position, blastRadius);
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].tag == "Player")
+            {
+                Player playerMovement = players[i].gameObject.GetComponent<Player>();
+                if (playerMovement != null)
+                {
+                    Debug.Log("Add Impact here");
+                    playerMovement.AddImpact(explosionForce, transform.position - players[i].transform.position);
+                }
+            }
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+}
